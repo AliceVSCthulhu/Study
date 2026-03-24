6140d9d6f03b8a5782c20a5fbbe6eb300899c051
AliceVSCthulhu <504677170@qq.com>
2026-03-24 17:59:45 +0800

    feat: 新增插值查找逻辑

---
+++ 算法基础/插值查找.js
@@ -0,0 +1,52 @@
+//插值查找数据集必须要是等差数列，这样计算的比例才有意义。若是非等差，则插值的效率不如预期。
+
+function isEqualDiffArr(arr) {
+  //长度为1，默认不可插值，故返回false
+  if (arr.length < 2) {
+    return false;
+  }
+  //长度大于1
+  let left = 0,
+    right = 1;
+  const gap = arr[1] - arr[0];
+  //do...while..循环逻辑有些臃肿啊
+  do {
+    left++;
+    right++;
+    if (right >= arr.length) break;
+    const gap_ = arr[right] - arr[left];
+    if (gap_ !== gap) {
+      return false;
+    }
+  } while (right < arr.length - 1);
+  return true;
+}
+
+function insertSearch(arr, target) {
+  //数组不为等差数列
+  if (!isEqualDiffArr(arr)) {
+    return -1;
+  }
+  let left = 0,
+    right = arr.length - 1;
+  //公差为0
+  if (arr[left] === arr[right]) {
+    return arr[left] === target ? left : -1;
+  }
+  //等差、长度大于1且公差不为0
+  while (left <= right && target >= arr[left] && target <= arr[right]) {
+    const mid =
+      left +
+      Math.floor(
+        ((target - arr[left]) / (arr[right] - arr[left])) * (right - left),
+      );
+    if (arr[mid] === target) {
+      return mid;
+    } else if (arr[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return -1;
+}
