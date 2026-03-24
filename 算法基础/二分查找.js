d683982d14e6f5421ba9019ef2fa992299c233f0
AliceVSCthulhu <504677170@qq.com>
2026-03-24 16:42:21 +0800

    feat: 新增二分法的前提条件 fix: 修复实现方式中函数调用

--- 算法基础/二分查找.js
+++ 算法基础/二分查找.js
@@ -1,8 +1,57 @@
+const supFunc = {
+  /**
+   * 验证参数是否为数组,且不为稀松数组,且每项不含null或undefined
+   * @param {} arr
+   * @returns Boolean
+   */
+  isNormalArray(arr) {
+    function isFullArray(arr) {
+      for (let i = 0; i < arr.length; i++) {
+        if (!(i in arr)) return false;
+      }
+      return true;
+    }
+    return (
+      // 遍历了三次数组，性能待优化
+      Array.isArray(arr) &&
+      isFullArray(arr) &&
+      !arr.some((ele) => ele === undefined || ele === null) &&
+      arr.every((ele) => !Number.isNaN(Number(ele.toString())))
+    );
+  },
+  /**
+   * 数组排序
+   * @param {} arr
+   * @param {Number} towards 1 (升序)||-1（降序）
+   */
+  mySort(arr, towards) {
+    if (!this.isNormalArray(arr)) {
+      throw new TypeError(
+        "Invalid First param value.It must be a non-empty array with each item being a number",
+      );
+    }
+    const towardsArr = [-1, 1];
+    if (!towardsArr.includes(towards)) {
+      throw new TypeError(
+        "Invalid second param value.Please enter the correct value. 1 represents forward direction, while -1 represents reverse direction",
+      );
+    }
+    // 次时双参状态必正确
+    if (towards > 0) {
+      arr.sort((a, b) => a - b);
+    } else {
+      arr.sort((a, b) => b - a);
+    }
+  },
+};
+
+const { isNormalArray, mySort } = supFunc;
 //二分法使用生成器实现
 function* BinarySearch(arr, target) {
-  if (!Array.isArray(arr)) {
+  if (!isNormalArray(arr)) {
     throw new TypeError("Param1 must be an Array!");
   }
+  mySort(arr, 1);
   let leftArrow = 0,
     rightArrow = arr.length - 1;
   while (leftArrow <= rightArrow) {
@@ -31,9 +80,10 @@ function* BinarySearch(arr, target) {
 
 //使用正常迭代器实现
 function BinarySearchNormal(arr, target) {
-  if (!Array.isArray(arr)) {
+  if (!isNormalArray(arr)) {
     throw new TypeError("Param1 must be an Array!");
   }
+  mySort(arr, 1);
   let left = 0;
   let right = arr.length - 1;
 
@@ -59,9 +109,10 @@ function BinarySearchRecursion(
   leftArrow = 0,
   rightArrow = arr.length - 1,
 ) {
-  if (!Array.isArray(arr)) {
+  if (!isNormalArray(arr)) {
     throw new TypeError("Param1 must be an Array!");
   }
+  mySort(arr, 1);
   const midArrow = Math.floor((leftArrow + rightArrow) / 2);
   const midVal = arr[midArrow];
   if (leftArrow > rightArrow) {
