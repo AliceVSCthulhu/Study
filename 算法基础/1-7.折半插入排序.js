fa7d2f580e94b49e4d2db88db095acf2b5a0c9f9
AliceVSCthulhu <504677170@qq.com>
2026-03-26 20:39:01 +0800

    feat: 新增了折半插入排序算法 (#8)
    * feat: 新增了折半插入排序算法
    
    * chore: 修改了curNum的声明方式

---
+++ 算法基础/1-7.折半插入排序.js
@@ -0,0 +1,39 @@
+import supFunc from "./supFunc.js";
+const { isNormalArray } = supFunc;
+
+function binaryInsertionSort(arr, forward = 1) {
+  if (!isNormalArray(arr)) {
+    throw new TypeError(
+      "The parameters must be a non-empty array consisting entirely of numbers",
+    );
+  }
+  const directions = [-1, 1];
+  if (!directions.includes(forward)) {
+    throw new RangeError("forward must be 1 or -1");
+  }
+  //双参无误
+  for (let i = 1; i < arr.length; i++) {
+    const curNum = arr[i];
+    //init
+    let left = 0,
+      right = i - 1,
+      mid = undefined;
+    //找mid
+    while (left <= right) {
+      mid = Math.floor((right + left) / 2);
+      // 处理排序(升序||降序)取值
+      if (forward === 1 ? arr[mid] <= curNum : arr[mid] >= curNum) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+    //找到边界left的值，移动arr下标大于left的元素
+    for (let j = i - 1; j >= left; j--) {
+      arr[j + 1] = arr[j];
+    }
+    // 插值
+    arr[left] = curNum;
+  }
+  return arr;
+}
